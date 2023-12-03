ec191f157dcefb25a72887f374c60a5327cf7533
Fin <finleywarman@hotmail.co.uk>
2023-12-03 14:38:48 +0000

    Add comments for part 2

--- 03/02.py
+++ 03/02.py
@@ -15,11 +15,15 @@ def load_input():
 
 # ==== SOLUTION ====
 
-num_re = re.compile(r'\b\d+\b')
+NUM_REGEX = re.compile(r'\d+')
 
+# check if any of a number is within range of a gear coordinate
 def is_within_distance(start_coord, length, target_coord):
-    x1, y1 = start_coord
-    x2, y2 = target_coord
+    y1, x1 = start_coord
+    y2, x2 = target_coord
+
+    if abs(y2 - y1) > 1 or abs(x1 - x2) > 3:
+        return False
 
     # check points along the x-axis (in length), at y, y+1, and y-1
     for i in range(length):
@@ -28,9 +32,10 @@ def is_within_distance(start_coord, length, target_coord):
                 return True
     return False
 
-def find_matches(array, regex):
+# find (row, col, length, value) of numbers in a (sub-)grid
+def find_numbers(arr, regex):
     matches = []
-    for i, text in enumerate(array):
+    for i, text in enumerate(arr):
         for match in regex.finditer(''.join(text)):
             start_index = match.start()
             match_length = match.end() - start_index
@@ -38,41 +43,42 @@ def find_matches(array, regex):
             matches.append((i, start_index, match_length, number))
     return matches
 
+# for the area around the current gear, get the neighbouring numbers
 def get_neighbour_numbers_count(grid, gear):
     row, col = gear
 
-    start_row = row - 1
-    end_row = row + 2
-    start_col = col - 3
-    end_col = col + 4
+    # get surrounding area of gear
+    start_row, end_row = row - 1, row + 2
+    start_col, end_col = col - 3, col + 4
+    GEAR_CENTRE = (1, 3) # relative to area
 
+    # find potential neighbouring numbers
     square = grid[start_row:end_row, start_col:end_col]
-    matches = find_matches(square, num_re)
+    matches = find_numbers(square, NUM_REGEX)
+
+    # determine actual neighbours
     neighbours = []
     for match in matches:
-        m_row, m_col, length, number = match
-        neighbour = is_within_distance((m_col, m_row), length, (3, 1))
-        if neighbour:
-            neighbours.append(number)
         if len(neighbours) > 2:
             break
+        m_row, m_col, length, number = match
+        neighbour = is_within_distance((m_row, m_col), length, GEAR_CENTRE)
+        if neighbour:
+            neighbours.append(number)
+
     return neighbours
 
 if __name__ == "__main__":
-    grid = np.pad(
-        load_input(),
-        pad_width=3, mode='constant', constant_values=' ',
-    )
+    grid = np.array(load_input())
+    potential_gears = list(zip(*np.where(grid == '*')))
 
-    row_indices, col_indices = np.where(grid == '*')
-    potential_gears = zip(row_indices, col_indices)
-
-    total = 0
+    # for each potential gear get the neighbouring numbers -
+    # for gears with two neighbours add the 'gear ratio' to the total
+    TOTAL = 0
     for gear in potential_gears:
         neighbours = get_neighbour_numbers_count(grid, gear)
         if len(neighbours) == 2:
-            gear_ratio = neighbours[0] * neighbours[1]
-            total += gear_ratio
+            TOTAL += neighbours[0] * neighbours[1]
 
-    print(total)
+    print(TOTAL)
     # 80403602
