0013f2c81f6d3467972086d76cacf82b86a7cbf0
Fin <finleywarman@hotmail.co.uk>
2023-12-04 10:04:06 +0000

    Pre-compute ranges, remove needless totals array

--- 04/02.py
+++ 04/02.py
@@ -25,19 +25,17 @@ for card_no, (winning, numbers) in enumerate(rows):
     win_count = len(set.intersection(winning, numbers))
     winning_counts[card_no] = win_count
 
-TOTAL = 0
+# pre-compute range map for adding back to stack
+range_map = [list(range(card+1, min(card+1+winning_counts[card], CARDS)))
+             for card in range(CARDS)]
 
 # resolve card counts
+TOTAL = 0
 stack = list(range(CARDS))
 while stack:
     TOTAL += 1
     card = stack.pop()
-    wins = winning_counts[card]
-    if wins > 0:
-        end_range = card + wins + 1
-        if end_range > CARDS:
-            end_range = CARDS
-        stack.extend(x for x in range(card+1, end_range))
+    stack.extend(range_map[card])
 
 print(TOTAL)
 
