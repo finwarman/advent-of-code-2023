53807351d50b55a9c724a0d631881e451850e4aa
Fin <finleywarman@hotmail.co.uk>
2023-12-04 10:16:00 +0000

    Use dynamic programming solution instead of stack-based

--- 04/02.py
+++ 04/02.py
@@ -1,6 +1,5 @@
 #! /usr/bin/env python3
 import re
-import math
 import numpy as np
 
 # ==== INPUT ====
@@ -16,27 +15,18 @@ CARDS = len(rows)
 # ==== SOLUTION ====
 
 # map: card numbers -> matching number count
-winning_counts = np.zeros(CARDS, dtype=int)
+winning_counts = np.ones(CARDS, dtype=int)
 
-# populate match count map
+# determine match counts, populate hits in each range
 for card_no, (winning, numbers) in enumerate(rows):
     winning = set(int(x) for x in re.split(r'\s+', winning))
     numbers = set(int(x) for x in re.split(r'\s+', numbers))
     win_count = len(set.intersection(winning, numbers))
-    winning_counts[card_no] = win_count
+    current_hits = winning_counts[card_no]
+    # each card in the range increases by the current card's hit count
+    for i in range(win_count):
+        winning_counts[(card_no+1) + i] += current_hits
 
-# pre-compute range map for adding back to stack
-range_map = [list(range(card+1, min(card+1+winning_counts[card], CARDS)))
-             for card in range(CARDS)]
-
-# resolve card counts
-TOTAL = 0
-stack = list(range(CARDS))
-while stack:
-    TOTAL += 1
-    card = stack.pop()
-    stack.extend(range_map[card])
-
-print(TOTAL)
+print(sum(winning_counts))
 
 # 5132675
