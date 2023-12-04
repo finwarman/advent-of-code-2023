fd13989b2b24e0df72cd1e7d4294a99d0264fd0c
Fin <finleywarman@hotmail.co.uk>
2023-12-04 09:48:29 +0000

    Add day 4 solutions

---
+++ 04/01.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python3
+import re
+import math
+
+# ==== INPUT ====
+
+INPUT = 'input.txt'
+with open(INPUT, 'r', encoding='UTF-8') as file:
+    data = file.read()
+
+rows = [re.split(r'\s+\|\s+', re.split(r':\s+', row.strip())[1])
+        for row in data.split('\n')[:-1]]
+
+# ==== SOLUTION ====
+
+TOTAL = 0
+for winning, numbers in rows:
+    winning = set(int(x) for x in re.split(r'\s+', winning))
+    numbers = set(int(x) for x in re.split(r'\s+', numbers))
+    winning_count = len(set.intersection(winning, numbers))
+    if winning_count:
+        TOTAL += 2**(winning_count-1)
+
+print(TOTAL)
+
+# 21959
---
+++ 04/02.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python3
+import re
+import math
+import numpy as np
+
+# ==== INPUT ====
+
+INPUT = 'input.txt'
+with open(INPUT, 'r', encoding='UTF-8') as file:
+    data = file.read()
+
+rows = [re.split(r'\s+\|\s+', re.split(r':\s+', row.strip())[1])
+        for row in data.split('\n')[:-1]]
+CARDS = len(rows)
+
+# ==== SOLUTION ====
+
+# map: card numbers -> matching number count
+winning_counts = np.array([0 for _ in range(CARDS)])
+
+# populate match count map
+for card_no, (winning, numbers) in enumerate(rows):
+    winning = set(int(x) for x in re.split(r'\s+', winning))
+    numbers = set(int(x) for x in re.split(r'\s+', numbers))
+    win_count = len(set.intersection(winning, numbers))
+    winning_counts[card_no] = win_count
+
+# map: card numbers -> number of 'hits'
+card_score_counts = np.array([0 for _ in range(CARDS)])
+
+# resolve card counts
+stack = list(range(CARDS))
+while stack:
+    card = stack.pop()
+    card_score_counts[card] += 1
+    wins = winning_counts[card]
+    if wins:
+        to_check = [x for x in range(card+1, min(CARDS, card+wins)+1)]
+        stack.extend(to_check)
+
+TOTAL = sum(card_score_counts)
+
+print(TOTAL)
+
+# 5132675
