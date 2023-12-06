55a18a778a628dd2ac4bfe91b4ecfc1ad1f0cbb7
Fin <finleywarman@hotmail.co.uk>
2023-12-06 10:02:16 +0000

    Use quadratic formula for part 2

--- 06/02.py
+++ 06/02.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 import re
 import numpy as np
+import math
 
 # ==== INPUT ====
 
@@ -12,25 +13,27 @@ rows = [row.strip() for row in data.split('\n')[:-1]]
 
 # ==== SOLUTION ====
 
-times        = [int(t) for t in [''.join(re.split(r'\s+', rows[0])[1:])]]
-record_dists = [int(d) for d in [''.join(re.split(r'\s+', rows[1])[1:])]]
+time_input  = [int(t) for t in [''.join(re.split(r'\s+', rows[0])[1:])]][0]
+record_dist = [int(d) for d in [''.join(re.split(r'\s+', rows[1])[1:])]][0]
 
-print(times)
+# use quadratic formula to find bounds
+# equation -x^2 + Tx - record = 0
+# (where x is the 'hold time', 'T' is the total time (constant))
+def find_bounds(time, record):
+    # coefficients
+    a, b, c = -1, time, -record
+    discriminant = b**2 - 4*a*c
 
-winning_counts = []
+    # calculate roots
+    root1 = (-b + math.sqrt(discriminant)) / (2*a)
+    root2 = (-b - math.sqrt(discriminant)) / (2*a)
 
-for i in range(len(times)):
-    time = times[i]
-    record = record_dists[i]
-    count = 0
-    for hold_time in range(time):
-        distance = hold_time * (time - hold_time)
-        if distance > record:
-            count += 1
-    winning_counts.append(count)
+    # get solutions counts
+    lower_bound = math.ceil(min(root1, root2))
+    upper_bound = math.floor(max(root1, root2))
 
+    return upper_bound - lower_bound + 1
 
-print(winning_counts)
-print(np.prod(winning_counts))
+print(find_bounds(time_input, record_dist))
 
 # 23501589
