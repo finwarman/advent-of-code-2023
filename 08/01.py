bdb3f324dd3105d12b12ceee2d73e55a98db8582
Fin <finleywarman@hotmail.co.uk>
2023-12-08 11:59:54 +0000

    Tidy part 1

--- 08/01.py
+++ 08/01.py
@@ -1,8 +1,4 @@
 #! /usr/bin/env python3
-import re
-import math
-
-from networkx import local_constraint
 
 # ==== INPUT ====
 
@@ -14,22 +10,19 @@ rows = [row.strip() for row in data.split('\n')[:-1]]
 
 # ==== SOLUTION ====
 
-left_right_pattern = [(0 if x == 'L' else 1) for x in rows[0]]
-left_right_pointer = 0
+lr_pattern = [(0 if x == 'L' else 1) for x in rows[0]]
 
 location_map = {}
 for row in rows[2:]:
     key, left_right = row.split(" = ")
-    left, right = left_right[1:-1].split(', ')
-    location_map[key] = (left, right)
+    location_map[key] = tuple(left_right[1:-1].split(', '))
 
-
-location = 'AAA'
+location, counter = ('AAA', 0)
 while location != 'ZZZ':
-    left_right = left_right_pattern[left_right_pointer % len(left_right_pattern)]
+    left_right = lr_pattern[counter % len(lr_pattern)]
     location = location_map[location][left_right]
-    left_right_pointer += 1
+    counter += 1
 
-print(left_right_pointer)
+print(counter)
 
 # 11309
