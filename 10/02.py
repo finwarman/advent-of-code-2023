4baf543d29ce5bf01ecae2398776cbe0fc92fdf4
Fin <finleywarman@hotmail.co.uk>
2023-12-11 12:25:04 +0000

    Refactor part 2

--- 10/02.py
+++ 10/02.py
@@ -16,13 +16,14 @@ GRID = np.array([list(row.strip()) for row in data.split('\n')[:-1]])
 
 # ==== SOLUTION ====
 
-# get valid chars in each direction
+# valid chars in each direction
 valid_direction_chars = {
     'up':    ('|', '7', 'F'),
     'down':  ('|', 'L', 'J'),
     'left':  ('-', 'L', 'F'),
     'right': ('-', '7', 'J'),
 }
+
 # map current char to possible directions
 valid_steps = {
     '.': (),
@@ -32,28 +33,21 @@ valid_steps = {
     'J': ('up',   'left'  ),
     '7': ('down', 'left'  ),
     'F': ('down', 'right' ),
-
-    # S = F
-    # 'S': ('down', 'right' ),
 }
-# map direction to (dx, dy) offset
+
+# map direction name to (dx, dy) offset
 offsets = {
     'up':    ( 0, -1),
     'down':  ( 0,  1),
     'left':  (-1,  0),
     'right': ( 1,  0),
 }
-opposites = {
-    'up':    'down' ,
-    'down':  'up'   ,
-    'left':  'right',
-    'right': 'left' ,
-}
 
-# add tuples (e.g. x,y + offset)
+# add tuples e.g. (x, y) + (dx, dy)
 def add(tuple_a, tuple_b):
     return (tuple_a[0]+tuple_b[0], tuple_a[1]+tuple_b[1])
 
+# get (x, y) positions for each direction name
 def get_neighbours(position):
     return {
         'up':    add(position, offsets['up']),
@@ -62,6 +56,17 @@ def get_neighbours(position):
         'right': add(position, offsets['right']),
     }
 
+# determine only valid char for current position
+def get_starting_char(start_pos):
+    x, y = start_pos
+    neighbour_chars = {dir: GRID[y][x] for dir, (x, y) in get_neighbours(start_pos).items()}
+    for potential, dirs in valid_steps.items():
+        valid = [char for dir, char in neighbour_chars.items() if dir in dirs and char in valid_direction_chars[dir]]
+        if len(valid) == 2:
+            return potential
+    return None
+
+# for current position, get valid steps from that position
 # [('down', (1, 2, '|')), ('right', (2, 1, '-'))]
 def get_possible_steps(position):
     x, y = position
@@ -78,45 +83,68 @@ def get_possible_steps(position):
 
     return possible_steps
 
+# return the set of values in the loop containing the given start coordinate
+def find_loop(start_coord):
+    queue, visited = deque([(start_coord, 0)]), {} # (x, y): dist
+    while queue:
+        (x, y), dist = queue.pop()
+        visited[(x, y)] = dist
 
-# TODO: add a border to the grid
-# Also, expand it in each direction to fill 'between the pipes'
+        possible_steps = get_possible_steps((x, y))
 
-# if either way is '-' or '|' then use that to fill the gap
-# TODO: later delete these columns
+        for _, (new_x, new_y), _ in possible_steps:
+            if (new_x, new_y) not in visited:
+                queue.append(((new_x, new_y), dist + 1))
 
+    return visited
+
+# flood fill outside of the loop, from the top-left of the border
+def fill_outside_of_loop(loop, fill_char='O'):
+    def flood_fill(x, y):
+        stack = [(x, y)]
+        while stack:
+            cx, cy = stack.pop()
+            if (cx, cy) in visited or GRID[cy][cx] != 'I':
+                continue
+            GRID[cy][cx] = fill_char
+            for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                nx, ny = cx + dx, cy + dy
+                if 0 <= nx < len(GRID[0]) and 0 <= ny < len(GRID):
+                    stack.append((nx, ny))
+
+    visited = set(loop)
+    exterior_point = (0, 0)
+    flood_fill(*exterior_point)
+
+# determine start position value
+# - use equivalent char for 'S' in mapping
+s_index = np.where(GRID == 'S')
+initial_start  = (s_index[1][0], s_index[0][0])
+
+START_CHAR = get_starting_char(initial_start)
+valid_steps['S'] = valid_steps[START_CHAR]
+
+# expand the entire grid to ensure an unfilled border
 new_grid = np.full((GRID.shape[0] + 2, GRID.shape[1] + 2), '.')
 new_grid[1:-1, 1:-1] = GRID
 GRID = new_grid
 
-
-# New grid with double the rows and columns
+# double the number of rows and columns (insert blank rows/cols)
+# (the ensures that 0-width gaps become flood-fillable)
 new_grid = np.full((GRID.shape[0] * 2, GRID.shape[1] * 2), '.')
-
-# Copy the original grid's contents into the new grid
 for i in range(GRID.shape[0]):
     for j in range(GRID.shape[1]):
         new_grid[i * 2, j * 2] = GRID[i, j]
-
 GRID = new_grid
 
-
-for row in GRID:
-    print(''.join(row))
-print()
-
-
-# find start position
+# find new start position after expanding
 s_index = np.where(GRID == 'S')
 START_POS  = (s_index[1][0], s_index[0][0])
 
-x, y = START_POS
-GRID[y][x] = 'F'
-# GRID[y][x] = '7'
+GRID[START_POS[1]][START_POS[0]] = START_CHAR
 
-# todo: determine start value
-
-# Iterate over new rows/cols and fill in blank
+# fill in gaps introduced by new rows/columns,
+# by extrapolating horiztonal/verticals with '-' and '|'
 for y in range(1, GRID.shape[0]-1, 2):
     for x in range(GRID.shape[1]):
         up   = GRID[y-1][x]
@@ -131,103 +159,26 @@ for x in range(1, GRID.shape[1]-1, 2):
         if 'right' in valid_steps[left] and 'left' in valid_steps[right]:
             GRID[y][x] = '-'
 
-for row in GRID:
-    print(''.join(row))
+main_loop = find_loop(START_POS)
 
-# TODO: use 'gap joining' to fill with lines for e.g. down and up matching cells
-
-
-
-
-def find_loop_for_coord(start_coord):
-    x, y = start_coord
-    start_char = GRID[y][x]
-
-    visited = {}
-    loop = []
-    queue = deque([(start_coord, start_char, 0)])
-
-    first = True
-
-    while queue:
-        (x, y), char, dist = queue.popleft()
-        if (x, y) not in visited:
-            visited[(x, y)] = dist
-            loop.append((x, y, char))
-
-            possible_steps = get_possible_steps((x, y))
-            if first:
-                possible_steps = [possible_steps[0]]
-                first = False
-
-            for _, (new_x, new_y), _ in possible_steps:
-                if (new_x, new_y) not in visited:
-                    queue.append(((new_x, new_y), GRID[new_y][new_x], dist + 1))
-    return loop
-
-main_loop = find_loop_for_coord(START_POS)
-
-# assign number to loop
-# for pos in main_loop:
-#     x, y, char = pos
-#     GRID[y][x] = 'A'
-#     print(pos)
-loop_pos = set([(x, y) for x, y, char in main_loop])
-
+# set every non-loop cell to filled (I, 'In')
 for y in range(len(GRID)):
     for x in range(len(GRID[0])):
-        if (x, y) not in loop_pos:
-            GRID[y][x]  = ' '
+        if (x, y) not in main_loop:
+            GRID[y][x]  = 'I'
 
-for row in GRID:
-    print(''.join(row))
+# fill everything outside of loop (O, 'Out')
+fill_outside_of_loop(main_loop)
 
-#  find all loops, and which loops each coord belong to
-
-# remove all 'junk' not in loops
-
-# todo:
-# get all loops
-# determine fill in the flood fill and remove the inner rings
-# starting with smaller rings?
-
-def mark_outside_of_loop(grid, loop):
-    def flood_fill(x, y):
-        stack = [(x, y)]
-        while stack:
-            cx, cy = stack.pop()
-            if (cx, cy) in visited or grid[cy][cx] != ' ':
-                continue
-            grid[cy][cx] = 'O'
-            for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-                nx, ny = cx + dx, cy + dy
-                if 0 <= nx < len(grid[0]) and 0 <= ny < len(grid):
-                    stack.append((nx, ny))
-
-    visited = set(loop)
-    exterior_point = (0, 0)
-    flood_fill(*exterior_point)
-
-mark_outside_of_loop(GRID, loop_pos)
-for row in GRID:
-    print(''.join(row))
-print()
-
-#  delete added rows and cols
-# Delete the new columns and rows
+# delete added rows and cols
+# (new border doesn't affect count, so is ignored)
 for index in range(GRID.shape[0] - 1, -1, -2):
     GRID = np.delete(GRID, index, axis=0)  # Delete row
 for index in range(GRID.shape[1] - 1, -1, -2):
     GRID = np.delete(GRID, index, axis=1)  # Delete column
 
-
-# Print the reverted grid
-for row in GRID:
-    print(''.join(row))
-
-
-count_i_cells = np.sum(GRID == ' ')
-print("Number of empty cells:", count_i_cells)
-
+# count number of of filled cells in original grid
+count_i_cells = np.sum(GRID == 'I')
+print(count_i_cells)
 
 # 349
