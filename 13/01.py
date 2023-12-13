f296d71b11b3513c33d1b6cbcb14abe9aa3f41a3
Fin <finleywarman@hotmail.co.uk>
2023-12-13 14:46:39 +0000

    Refactor part 1

--- 13/01.py
+++ 13/01.py
@@ -8,20 +8,13 @@ INPUT = 'input.txt'
 with open(INPUT, 'r', encoding='UTF-8') as file:
     data = file.read()
 
-
+data = data.replace('#', '1').replace('.', '0')
 sections = [row.strip()  for row in data.split('\n\n')]
-grids = [np.array([list(row)for row in section.split('\n')]) for section in sections]
+grids = [np.array([[int(x) for x in list(row)] for row in section.split('\n')]) for section in sections]
 
 # ==== SOLUTION ====
 
-col_count = 0
-row_count = 0
-
-for grid in grids:
-    rows, cols = grid.shape
-    # h_flip = np.flip(grid, axis=1)
-    h_done = False
-
+def array_flip(grid, cols):
     for i in range(1, cols):
         if i > cols//2:
             window = cols-i
@@ -31,20 +24,22 @@ for grid in grids:
         left_segment  = grid[:, i-window:i]
         right_segment = np.flip(grid[:, i:i+window], axis=1)
         if np.array_equal(left_segment, right_segment):
-                col_count += i
-                break
+            return i
+    return None
 
-    if h_done:
+col_count, row_count = 0, 0
+
+for grid in grids:
+    rows, cols = grid.shape
+
+    h_count = array_flip(grid, cols)
+    if h_count is not None:
+        col_count += h_count
         continue
 
-    # v_flip = np.flip(grid, axis=0)
-    for i in range(1, rows):
-        window = min(i, rows - i)
-        top_segment = grid[i-window:i, :]
-        bottom_segment = np.flip(grid[i:i+window, :], axis=0)
-        if np.array_equal(top_segment, bottom_segment):
-            row_count += i
-            break
+    v_count = array_flip(grid.T, rows)
+    if v_count is not None:
+        row_count += v_count
 
 
 total = col_count + (100 * row_count)
