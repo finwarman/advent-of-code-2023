e685830a13025ad90734c1f0e7ac583e4c2ccc89
Fin <finleywarman@hotmail.co.uk>
2023-12-15 10:56:07 +0000

    Add part 1 solution

---
+++ 15/01.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python3
+
+# ==== INPUT ====
+
+INPUT = 'input.txt'
+# INPUT = 'example.txt'
+with open(INPUT, 'r', encoding='UTF-8') as file:
+    data = file.read()
+
+input = data.strip()
+steps = input.split(',')
+
+# ==== SOLUTION ====
+
+# print(input)
+
+grand_total = 0
+for step in steps:
+    total = 0
+    for char in step:
+        total = ((total + ord(char)) * 17) % 256
+    grand_total += total
+
+print(grand_total)
+
+# 516070
